adae7b4e4e61c82b721806f5fe7db89ece494efd
Arthur <arthur@corenzan.com>
2015-11-26 17:43:19 -0200

    Fix making inputs editable again

--- readonly.js
+++ readonly.js
@@ -1,65 +1,62 @@
 /**
- * Readonly v2.0.0
+ * Readonly v2.0.1
  * by Arthur Corenzan <arthur@corenzan.com>
  * more on https://github.com/haggen/readonly
  */
 ;(function(undefined) {
 
-  function readonly(target, value) {
+  var readonly = function(target) {
+    var sham = document.createElement('input');
+
+    sham.name = target.name;
+    sham.type = 'hidden';
+    sham.value = target.value;
+    sham.setAttribute('data-sham', target.name);
+
+    target.setAttribute('disabled', true);
+    target.setAttribute('readonly', true);
+    target.classList.add('readonly');
+    target.parentNode.insertBefore(sham, target.nextSibling);
+  };
+
+  var editable = function(target) {
+    target.removeAttribute('disabled');
+    target.removeAttribute('readonly');
+    target.classList.remove('readonly');
+
+    if(target.nextSibling.getAttribute('data-sham') === target.name) {
+      target.parentNode.removeChild(target.nextSibling);
+    }
+  };
+
+  var toggle = function(target, value) {
+    console.log(target, value)
+
     if(typeof target === 'string') {
       target = document.querySelectorAll(target);
     }
 
-    if(target.length === undefined) {
+    if(target instanceof HTMLElement) {
       target = [target];
     }
 
     [].forEach.call(target, function(el) {
-      var n = value === undefined ? !el.getAttribute('readonly') : value;
-      readonly[n ? 'activate' : 'deactivate'](el);
+      if(value === undefined ? !el.getAttribute('data-readonly') : value) {
+        readonly(el);
+      } else {
+        editable(el);
+      }
     });
   }
-
-  readonly.activate = function(el) {
-    el.setAttribute('readonly', true);
-    el.setAttribute('disabled', true);
-
-    el.classList.add('readonly');
-
-    var checkable = /checkbox|radio/i.test(el.type);
-
-    console.log(el.nodeName, checkable);
-
-    if(!checkable || (checkable && el.checked)) {
-      var sham = document.createElement('input');
-
-      sham.name = el.name;
-      sham.type = 'hidden';
-      sham.value = el.value;
-      sham.setAttribute('data-sham', el.name);
-
-      el.parentNode.insertBefore(sham, el.nextSibling);
-    }
-  };
-
-  readonly.deactivate = function(el) {
-    el.setAttribute('readonly', false);
-    el.setAttribute('disabled', false);
-
-    el.classList.remove('readonly');
-
-    var sham = el.parentNode.querySelector('[data-sham="' + el.name + '"]');
-    if(sham) el.parentNode.removeChild(sham);
-  };
-
-  this.readonly = readonly;
 
   if(this.jQuery !== undefined) {
     this.jQuery.fn.readonly = function(value) {
       return this.each(function() {
-        readonly(this, value);
+        toggle(this, value);
       });
     };
   }
+
+  this.readonly = toggle;
 
 })(this);
