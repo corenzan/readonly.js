d6382a9f6ce02dbc366fc00ea2b0cc659d8995d2
Arthur <arthur@corenzan.com>
2018-07-10 00:03:27 -0300

    Include distribution file

---
+++ dist/readonly.js
@@ -0,0 +1,126 @@
+// Readonly.js 3.0.0
+// The MIT License © 2013 Arthur Corenzan
+// More on https://github.com/corenzan/readonly.js
+!function (root, undefined) {
+  var typesThatNeedSurrogate = ['checkbox', 'range', 'radio'];
+
+  var allowedNodeNames = ['INPUT', 'SELECT', 'TEXTAREA'];
+
+  var isCheckable = function isCheckable(target) {
+    return target.type === 'checkbox' || target.type === 'radio';
+  };
+
+  var supportsReadOnly = function supportsReadOnly(target) {
+    return 'readOnly' in target && typesThatNeedSurrogate.indexOf(target.type) < 0;
+  };
+
+  var addSurrogate = function addSurrogate(target) {
+    var surrogate = document.createElement('input');
+    surrogate.type = 'hidden';
+
+    surrogate.sync = function (e) {
+      surrogate.name = target.name;
+      surrogate.value = target.value;
+
+      if (isCheckable(target)) {
+        surrogate.disabled = !target.checked;
+      }
+    };
+
+    surrogate.sync();
+    target.addEventListener('change', surrogate.sync);
+
+    target.surrogate = surrogate;
+    target.parentElement.insertBefore(surrogate, target.nextElementSibling);
+  };
+
+  var removeSurrogate = function removeSurrogate(target) {
+    target.removeEventListener('change', target.surrogate.sync);
+    target.parentElement.removeChild(target.surrogate);
+    delete target.surrogate;
+  };
+
+  var setReadOnly = function setReadOnly(target) {
+    if (target.hasAttribute('readonly')) {
+      return;
+    }
+
+    if (supportsReadOnly(target)) {
+      target.readOnly = true;
+      return;
+    }
+
+    if (!target.parentElement) {
+      throw Error('readonly-js: control needs a surrogate but has not been inserted into a dom tree yet, i.e. has no parent element');
+    }
+
+    target.setAttribute('disabled', '');
+    target.setAttribute('readonly', '');
+
+    addSurrogate(target);
+  };
+
+  var unsetReadOnly = function unsetReadOnly(target) {
+    if (!target.hasAttribute('readonly')) {
+      return;
+    }
+
+    if (supportsReadOnly(target)) {
+      target.readOnly = false;
+      return;
+    }
+
+    if (!target.parentElement) {
+      throw Error('readonly-js: control needs a surrogate but has not been inserted into a dom tree yet, i.e. has no parent element');
+    }
+
+    target.removeAttribute('disabled');
+    target.removeAttribute('readonly');
+
+    removeSurrogate(target);
+  };
+
+  var toggleReadOnly = function toggleReadOnly(target, value) {
+    if (value === undefined) {
+      value = !target.hasAttribute('readonly');
+    }
+
+    if (value) {
+      setReadOnly(target);
+    } else {
+      unsetReadOnly(target);
+    }
+  };
+
+  var entryPoint = function entryPoint(mixed, value) {
+    if (typeof mixed === 'string') {
+      mixed = document.querySelectorAll(mixed);
+    } else if (mixed instanceof NodeList) {
+      mixed = Array.from(mixed);
+    } else if (mixed instanceof HTMLElement) {
+      mixed = [mixed];
+    } else {
+      throw Error('readonly-js: invalid argument');
+    }
+
+    mixed.forEach(function (target) {
+      if (allowedNodeNames.indexOf(target.nodeName) < 0) {
+        throw Error('readonly-js: element ' + target.nodeName + ' is not allowed');
+      }
+
+      toggleReadOnly(target);
+    });
+  };
+
+  if ('jQuery' in root) {
+    root.jQuery.fn.readonly = function (value) {
+      var _this = this;
+
+      return this.each(function () {
+        entryPoint(_this, value);
+      });
+    };
+  }
+
+  root.readonly = entryPoint;
+}(this);
